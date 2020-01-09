f090bb7d504f1ecca3f32b654e7470777bffec41
Steven Paulino <stevenpaulino1@hotmail.com>
2020-01-09 11:21:23 -0500

    created config, reuseable widget

---
+++ miniApps/movieApp/autocomplete.js
@@ -0,0 +1,61 @@
+const creatAutoComplete = ({
+  root,
+  renderOption,
+  onOptionSelect,
+  inputValue,
+  fetchData
+}) => {
+  root.innerHTML = `
+  <label>
+    <b>
+      Search
+    </b>
+  </label>
+  <input class="input"></input>
+  <div class= "dropdown">
+  <div class= "dropdown-menu">
+  <div class="dropdown-content results"></div>
+  </div>
+  </div>
+  `;
+
+  const inputEl = root.querySelector("input");
+  const dropdown = root.querySelector(".dropdown");
+  const resultsWrapper = root.querySelector(".results");
+
+  const onInput = async event => {
+    // console.log(event.target);
+    const items = await fetchData(event.target.value.trim());
+    // console.log(items);
+
+    if (!items) {
+      dropdown.classList.remove("is-active");
+      return;
+    }
+
+    resultsWrapper.innerHTML = "";
+
+    dropdown.classList.add("is-active");
+
+    for (let item of items) {
+      const option = document.createElement("a");
+      option.classList.add("dropdown-item");
+
+      option.innerHTML = renderOption(item);
+      option.addEventListener("click", () => {
+        dropdown.classList.remove("is-active");
+        inputEl.value = inputValue(item);
+        onOptionSelect(item);
+      });
+      resultsWrapper.appendChild(option);
+    }
+  };
+
+  inputEl.addEventListener("input", debounce(onInput));
+
+  document.addEventListener("click", event => {
+    if (!root.contains(event.target)) {
+      dropdown.classList.remove("is-active");
+    }
+  });
+};
--- miniApps/movieApp/index.js
+++ miniApps/movieApp/index.js
@@ -1,73 +1,29 @@
-fetchData = async searchTerm => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: "8f29224d",
-      s: searchTerm
-    }
-  });
-  if (response.data.error) {
-    return [];
-  }
-  return response.data.Search;
-};
-
-const autocompleteDiv = document.querySelector(".autocomplete");
-autocompleteDiv.innerHTML = `
-<label>
-  <b>
-    Search For a Movie
-  </b>
-</label>
-<input class="input"></input>
-<div class= "dropdown">
-<div class= "dropdown-menu">
-<div class="dropdown-content results"></div>
-</div>
-</div>
-`;
-
-const inputEl = document.querySelector("input");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-const onInput = async event => {
-  console.log(event.target);
-  const movies = await fetchData(event.target.value.trim());
-  console.log(movies);
-
-  if (!movies) {
-    dropdown.classList.remove("is-active");
-    return;
-  }
-
-  resultsWrapper.innerHTML = "";
-
-  dropdown.classList.add("is-active");
-
-  for (let movie of movies) {
-    const option = document.createElement("a");
+creatAutoComplete({
+  root: document.querySelector(".autocomplete"),
+  renderOption(movie) {
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
-    option.classList.add("dropdown-item");
-
-    option.innerHTML = `
-  <img src="${imgSrc}" />
-  ${movie.Title}
-  `;
-    option.addEventListener("click", () => {
-      // console.log("AFTER CKICK", inputEl, movie.Title);
-      dropdown.classList.remove("is-active");
-      inputEl.value = movie.Title;
-      onMovieSelect(movie);
+    return `
+              <img src="${imgSrc}" />
+              ${movie.Title} ${movie.Year}
+            `;
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "8f29224d",
+        s: searchTerm
+      }
     });
-    resultsWrapper.appendChild(option);
-  }
-};
-
-inputEl.addEventListener("input", debounce(onInput));
-
-document.addEventListener("click", event => {
-  if (!autocompleteDiv.contains(event.target)) {
-    dropdown.classList.remove("is-active");
+    if (response.data.error) {
+      return [];
+    }
+    return response.data.Search;
   }
 });
 
