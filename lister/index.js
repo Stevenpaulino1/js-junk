690d91db2cca8ef11290264403b0e031660dbb8e
Steven Paulino <stevenpaulino1@hotmail.com>
2020-01-13 17:52:08 -0500

    node scripting

---
+++ lister/index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+
+fs.readdir(process.cwd(), (err, filenames) => {
+  if (err) {
+    console.log(err);
+  }
+
+  //bad way
+  for (let filename of filenames) {
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(filename, stats.isFile());
+    });
+  }
+});
