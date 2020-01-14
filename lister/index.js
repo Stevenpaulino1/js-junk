d40aea04f2d3d78719852547a139c91732519cee
Steven Paulino <stevenpaulino1@hotmail.com>
2020-01-14 13:45:21 -0500

    command line ls tool working

--- lister/index.js
+++ lister/index.js
@@ -1,19 +1,51 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const path = require("path");
 
-fs.readdir(process.cwd(), (err, filenames) => {
+// const lstat = util.promisify(fs.lstat);
+
+const targetDir = process.argv[2] || process.cwd();
+
+const { lstat } = fs.promises;
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
   }
+  //
+  // for (let filename of filenames) {
+  //   try {
+  //     const stats = await lstat(filename);
+  //   } catch (err) {
+  //     console.log("err", err);
+  //   }
+  // }
 
-  //bad way
-  for (let filename of filenames) {
-    fs.lstat(filename, (err, stats) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(filename, stats.isFile());
-    });
+  const statPromises = filenames.map(filename => {
+    // console.log("WHAT", targetDir, filename);
+    return lstat(path.join(targetDir, filename));
+  });
+  const allStats = await Promise.all(statPromises);
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    if (stats.isFile()) {
+      console.log(chalk.gray(filenames[index]));
+    } else {
+      console.log(chalk.bold(filenames[index]));
+    }
   }
 });
+
+// const lstat = filename => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(stats);
+//     });
+//   });
+// };
